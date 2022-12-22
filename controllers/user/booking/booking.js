d9f173ce7bb46d84ec9e89a7a1bc59fc9a299e2a
razscalelot <you@example.com>
2022-12-22 17:33:56 +0530

    booking api for testing

--- controllers/user/booking/booking.js
+++ controllers/user/booking/booking.js
@@ -9,84 +9,8 @@ exports.booking = async (req, res) => {
         let primary = mongoConnection.useDb(constants.DEFAULT_DB);
         let userdata = await primary.model(constants.MODELS.users, userModel).findById(req.token.userid).lean();
         if (userdata && userdata.status == true && userdata.mobileverified == true) {
-            const { bookingid, eventid, start_date, end_date, start_time, end_time } = req.body;
-            if (bookingid && bookingid != '' && mongoose.Types.ObjectId.isValid(bookingid)){
-                if (eventid && eventid != '' && mongoose.Types.ObjectId.isValid(eventid)) {
-                    if (start_date && start_date != '' && end_date && end_date != '') {
-                        if (start_time && start_time != '' && end_time && end_time != '') {
-                            let xstart_date = start_date.split("-");
-                            let startTimestamp = new Date(xstart_date[1] + '-' + xstart_date[2] + '-' + xstart_date[0] + ' ' + start_time).getTime();
-                            let yend_date = end_date.split("-");
-                            let endTimestamp = new Date(yend_date[1] + '-' + yend_date[2] + '-' + yend_date[0] + ' ' + end_time).getTime();
-                            let existingBookingEvent = await primary.model(constants.MODELS.eventbookings, eventbookingModel).findOne({ eventid: mongoose.Types.ObjectId(eventid), start_date: start_date, end_date: end_date, start_time: start_time, end_time: end_time}).lean();
-                            if (existingBookingEvent == null) {
-                                let obj = {
-                                    start_date: start_date,
-                                    end_date: end_date,
-                                    start_time: start_time,
-                                    end_time: end_time,
-                                    about_event: about_event,
-                                    start_timestamp: startTimestamp,
-                                    end_timestamp: endTimestamp,
-                                };
-                                await primary.model(constants.MODELS.eventbookings, eventbookingModel).findByIdAndUpdate(bookingid, obj);
-                                let bookingEventData = await primary.model(constants.MODELS.eventbookings, eventbookingModel).findById(bookingid).lean();
-                                if(bookingEventData && bookingEventData != null){
-                                    return responseManager.onSuccess('Event Book successfully!', bookingEventData, res);
-                                }else{
-                                    return responseManager.badrequest({ message: 'Invalid event id get event data, please try again' }, res);
-                                }
-                            }else{
-                                return responseManager.badrequest({message : 'On this date and time event already booked, Please try again...'}, res);
-                            }
-                        } else {
-                            return responseManager.badrequest({ message: 'Invalid start or end time for event about data, please try again' }, res);
-                        }
-                    } else {
-                        return responseManager.badrequest({ message: 'Invalid start or end date for event about data, please try again' }, res);
-                    }
-                } else {
-                    return responseManager.badrequest({ message: 'Invalid event id to get event data, please try again' }, res);
-                }
-            }else{
-                if (eventid && eventid != '' && mongoose.Types.ObjectId.isValid(eventid)) {
-                    if (start_date && start_date != '' && end_date && end_date != '') {
-                        if (start_time && start_time != '' && end_time && end_time != '') {
-                            let xstart_date = start_date.split("-");
-                            let startTimestamp = new Date(xstart_date[1] + '-' + xstart_date[2] + '-' + xstart_date[0] + ' ' + start_time).getTime();
-                            let yend_date = end_date.split("-");
-                            let endTimestamp = new Date(yend_date[1] + '-' + yend_date[2] + '-' + yend_date[0] + ' ' + end_time).getTime();
-                            let existingBookingEvent = await primary.model(constants.MODELS.eventbookings, eventbookingModel).findOne({ eventid: mongoose.Types.ObjectId(eventid), start_date: start_date, end_date: end_date, start_time: start_time, end_time: end_time}).lean();
-                            if (existingBookingEvent && existingBookingEvent != null) {
-                                let obj = {
-                                    start_date: start_date,
-                                    end_date: end_date,
-                                    start_time: start_time,
-                                    end_time: end_time,
-                                    about_event: about_event,
-                                    start_timestamp: startTimestamp,
-                                    end_timestamp: endTimestamp,
-                                };
-                                await primary.model(constants.MODELS.eventbookings, eventbookingModel).create(obj);
-                                let bookingEventData = await primary.model(constants.MODELS.eventbookings, eventbookingModel).findById(bookingid).lean();
-                                if(bookingEventData && bookingEventData != null){
-                                    return responseManager.onSuccess('Event Book successfully!', bookingEventData, res);
-                                }else{
-                                    return responseManager.badrequest({ message: 'Invalid event id get event data, please try again' }, res);
-                                }
-                            }else{
-                                return responseManager.badrequest({message : 'On this date and time event already booked, Please try again...'}, res);
-                            }
-                        } else {
-                            return responseManager.badrequest({ message: 'Invalid start or end time for event about data, please try again' }, res);
-                        }
-                    } else {
-                        return responseManager.badrequest({ message: 'Invalid start or end date for event about data, please try again' }, res);
-                    }
-                } else {
-                    return responseManager.badrequest({ message: 'Invalid event id to get event data, please try again' }, res);
-                }
-            }
+            // const { bookingid, eventid, start_date, end_date, start_time, end_time } = req.body;
+            let createdBooking = await primary.model(constants.MODELS.eventbookings, eventbookingModel).create(req.body);
         } else {
             return responseManager.badrequest({ message: 'Invalid user id to book event data, please try again' }, res);
         }
