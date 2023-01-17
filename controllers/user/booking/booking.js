2ee0d4647144291828a019092c271c228c69d47c
razscalelot <you@example.com>
2023-01-17 12:55:06 +0530

    update puppeteer

--- controllers/user/booking/booking.js
+++ controllers/user/booking/booking.js
@@ -295,6 +295,7 @@ exports.booking = async (req, res) => {
                                             url: result.data.Key
                                         };
                                         primary.model(constants.MODELS.eventbookings, eventbookingModel).findByIdAndUpdate(output._id, { invoice_url: result.data.Key }).then((updateResult) => {
+                                            console.log("updateResult", updateResult);
                                             return responseManager.onSuccess('Booking successfully... Donwload the Invoice !', updateResult, res);
                                         }).catch((error) => {
                                             console.log("293", error);
