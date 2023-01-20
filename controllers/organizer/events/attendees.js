06b80d42cbb501e5030fb18c2fba34e1eeb10b44
razscalelot <you@example.com>
2023-01-20 18:26:18 +0530

    event data can not edit as event booking started api

--- controllers/organizer/events/attendees.js
+++ controllers/organizer/events/attendees.js
@@ -55,10 +55,10 @@ exports.export = async (req, res) => {
         let primary = mongoConnection.useDb(constants.DEFAULT_DB);
         let organizerData = await primary.model(constants.MODELS.organizers, organizerModel).findById(req.token.organizerid).select('-password').lean();
         if (organizerData && organizerData.status == true && organizerData.mobileverified == true && organizerData.is_approved == true) {
-            if(eventid && eventid != '' && mongoose.Types.ObjectId.isValid(eventid)){
+            if (eventid && eventid != '' && mongoose.Types.ObjectId.isValid(eventid)) {
                 let eventData = await primary.model(constants.MODELS.events, eventModel).findById(eventid).lean();
-                if(eventData && eventData.is_approved == true && eventData.status == true && eventData.createdBy.toString() == req.token.organizerid.toString()){
-                    let attendeelist = await primary.model(constants.MODELS.eventbookings, eventbookingModel).find({eventId : mongoose.Types.ObjectId(eventid)}).populate({path: 'userid', model: primary.model(constants.MODELS.users, userModel), select: "name email mobile"}).lean();
+                if (eventData && eventData.is_approved == true && eventData.status == true && eventData.createdBy.toString() == req.token.organizerid.toString()) {
+                    let attendeelist = await primary.model(constants.MODELS.eventbookings, eventbookingModel).find({ eventId: mongoose.Types.ObjectId(eventid) }).populate({ path: 'userid', model: primary.model(constants.MODELS.users, userModel), select: "name email mobile" }).lean();
                     var ext = 'xlsx';
                     var date = new Date();
                     var timestamp = date.getTime().toString();
@@ -143,36 +143,21 @@ exports.export = async (req, res) => {
                         let totalseats = 0;
                         let seatsdetails = "";
                         let obj = {
-                            invoice_no : attendee.invoice_no,
-                            attendee_name : attendee.userid.name,
-                            event_name : eventData.display_name,
-                            payment_id : attendee.payment_id,
-                            status : attendee.status,
-                            subTotal : attendee.subTotal,
-                            couponAmount : attendee.couponAmount,
-                            finalTotal : attendee.finalTotal,
-                            discountOnTotalBill : attendee.discountOnTotalBill,
-                            amount : attendee.amount,
-                            timestamp : new Date(attendee.timestamp),
-                            invoice_url : process.env.AWS_BUCKET_URI+attendee.invoice_url,
-                            totalseats : totalseats,
-                            seatsdetails : seatsdetails
+                            invoice_no: attendee.invoice_no,
+                            attendee_name: attendee.userid.name,
+                            event_name: eventData.display_name,
+                            payment_id: attendee.trans_Id,
+                            status: attendee.status,
+                            subTotal: attendee.subTotal,
+                            couponAmount: attendee.couponPrice,
+                            finalTotal: attendee.totalPrice,
+                            discountOnTotalBill: attendee.discountOnTotalBill,
+                            amount: attendee.amount,
+                            timestamp: new Date(attendee.timestamp),
+                            invoice_url: process.env.AWS_BUCKET_URI + attendee.invoice_url,
                         };
-                        async.forEachSeries(attendee.seats, (seat, next_seat) => {
-                            async.forEachSeries(seat.arrangements, (arrangement, next_arrangement) => {
-                                let eqpincluded = (seat.equipment == true) ? "Included" : "Not-Included";
-                                totalseats += parseInt(arrangement.ItemCount);
-                                seatsdetails += " ( "+seat.seating_item.itemname+" | "+arrangement.vertical_location+" - "+arrangement.horizontal_location+" | "+arrangement.ItemCount+" | "+" FOOD - "+seat.food+" | "+" Eqp. - "+ eqpincluded + " ) ";
-                                next_arrangement();
-                            }, () => {
-                                next_seat();
-                            });
-                        }, () => {
-                            obj.totalseats = totalseats;
-                            obj.seatsdetails = seatsdetails;
-                            sheet1.addRow(obj);
-                            next_attendee();
-                        });
+                        sheet1.addRow(obj);
+                        next_attendee();
                     }, () => {
                         workbook.xlsx.writeFile(excelFileName).then(() => {
                             var data = fs.readFileSync(excelFileName);
@@ -185,16 +170,16 @@ exports.export = async (req, res) => {
                             }
                         });
                     });
-                }else{
+                } else {
                     return responseManager.badrequest({ message: 'Invalid eventid to get event attendees export report, please try again' }, res);
                 }
-            }else{
+            } else {
                 return responseManager.badrequest({ message: 'Invalid eventid to get event attendees export report, please try again' }, res);
             }
-        }else{
+        } else {
             return responseManager.badrequest({ message: 'Invalid organizerid to get event attendees export report, please try again' }, res);
         }
-    }else{
+    } else {
         return responseManager.unauthorisedRequest(res);
     }
 };
