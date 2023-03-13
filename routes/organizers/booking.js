97ecd0531d5a1e5fd386ff2f470026e9d7df7ed5
razscalelot <you@example.com>
2023-03-13 16:08:44 +0530

    booking filter changes

--- routes/organizers/booking.js
+++ routes/organizers/booking.js
@@ -11,53 +11,52 @@ const userModel = require('../../models/users.model');
 const async = require("async");
 const mongoose = require('mongoose');
 router.post('/list', helper.authenticateToken, async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
     if (req.token.organizerid && mongoose.Types.ObjectId.isValid(req.token.organizerid)) {
+        const { date, time, event_type, search } = req.body;
         let primary = mongoConnection.useDb(constants.DEFAULT_DB);
-        let organizerdata = await primary.model(constants.MODELS.organizers, organizerModel).findById(req.token.organizerid).lean();
-        if (organizerdata && organizerdata.status == true && organizerdata.mobileverified == true) {
-            const { page, limit, date, time, event_type } = req.body;
-            let primary = mongoConnection.useDb(constants.DEFAULT_DB);
-            let eventData = await primary.model(constants.MODELS.events, eventModel).find({ createdBy: mongoose.Types.ObjectId(req.token.organizerid) }).select("_id").lean();
-            let allEventsId = [];
-            async.forEachSeries(eventData, (event, next_booking) => {
-                if (event._id && event._id != '' && mongoose.Types.ObjectId.isValid(event._id)) {
-                    allEventsId.push(mongoose.Types.ObjectId(event._id));
+        let organizerData = await primary.model(constants.MODELS.organizers, organizerModel).findById(req.token.organizerid).select('-password').lean();
+        if (organizerData && organizerData.status == true && organizerData.mobileverified == true && organizerData.is_approved == true) {
+            let bookingquery = {};
+            let eventquery = {};
+            if (date) {
+                if (time && time != '') {
+                    let starttimestamp = new Date(date + ' ' + time).getTime()  + 19800000;
+                    bookingquery.created_at = { "$gte": starttimestamp };
+                } else {
+                    let starttimestamp = new Date(date + ' 00:00').getTime()  + 19800000;
+                    bookingquery.created_at = { "$gte": starttimestamp };
                 }
-                next_booking();
-            });
-            primary.model(constants.MODELS.eventbookings, eventbookingModel).paginate({
-                eventId: { $in: allEventsId }, 
-                $or: [
-                    { start_date: { '$regex': new RegExp(date, "i") } },
-                ],
-                $or: [
-                    { start_time: { '$regex': new RegExp(time, "i") } },
-                ],
-                $or: [
-                    { event_type: { '$regex': new RegExp(event_type, "i") } },
-                ]
-            }, {
-                page,
-                limit: parseInt(limit),
-                sort: { _id: -1 },
-                populate: {
-                    path: 'userid',
-                    model: primary.model(constants.MODELS.users, userModel),
-                    select : 'name profile_pic'
-                },                
-                // select: '_id userid eventId trans_Id name url category_name address payment_status createdAt updatedAt selectedItems selectedEquipments selectedServices totalPrice start_date end_date start_time end_time start_timestamp end_timestamp',
-                lean: true
-            }).then((eventBookingData) => {
-                return responseManager.onSuccess('Booking list data!', eventBookingData, res);
-            }).catch((error) => {
-                return responseManager.onError(error, res);
+            }
+            if (event_type && event_type != '') {
+                eventquery.event_type = event_type;
+            }
+            let allEvents = await primary.model(constants.MODELS.events, eventModel).find({ createdBy: mongoose.Types.ObjectId(req.token.organizerid), ...eventquery }).select('display_name').lean();
+            let eventIds = [];
+            async.forEachSeries(allEvents, (event, next_event) => {
+                eventIds.push(mongoose.Types.ObjectId(event._id));
+                next_event();
+            }, () => {
+                (async () => {
+                    let bookedEvenets = await primary.model(constants.MODELS.eventbookings, eventbookingModel).find({
+                        $or: [
+                            { name: { '$regex': new RegExp(search, "i") } },
+                            { start_date: { '$regex': new RegExp(search, "i") } },
+                            { end_date: { '$regex': new RegExp(search, "i") } },
+                            { invoice_no: { '$regex': new RegExp(search, "i") } }
+                        ],
+                        eventId: { $in: eventIds },
+                        ...bookingquery
+                    }).populate([{ path: 'userid', model: primary.model(constants.MODELS.users, userModel), select: 'name email mobile country_code profilepic' }]).sort({ _id: -1 }).lean();
+                    return responseManager.onSuccess("event booked list...", bookedEvenets, res);
+                })().catch((error) => {
+                    return responseManager.onError(error, res);
+                });
             });
         } else {
-            return responseManager.badrequest({ message: 'Invalid organizerid to get event discount details, please try again' }, res);
+            return responseManager.badrequest({ message: 'Invalid organizerid to get booking list, please try again' }, res);
         }
     } else {
-        return responseManager.badrequest({ message: 'Invalid token to get event data, please try again' }, res);
+        return responseManager.unauthorisedRequest(res);
     }
 });
 module.exports = router;
