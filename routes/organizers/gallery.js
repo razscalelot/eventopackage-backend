2f708ab90a8f64f831e57f5cb0d1938293aae735
razscalelot <you@example.com>
2023-04-27 09:29:39 +0530

    is_live event images and video on entertainment

--- routes/organizers/gallery.js
+++ routes/organizers/gallery.js
@@ -17,7 +17,7 @@ router.get('/', helper.authenticateToken, async (req, res) => {
         let organizerData = await primary.model(constants.MODELS.organizers, organizerModel).findById(req.token.organizerid).select('-password').lean();
         if (organizerData && organizerData.status == true && organizerData.mobileverified == true && organizerData.is_approved == true) {
             let primary = mongoConnection.useDb(constants.DEFAULT_DB);
-            let imagesvideos = await primary.model(constants.MODELS.events, eventModel).find({ createdBy: { $ne: mongoose.Types.ObjectId(req.token.organizerid) } }).populate([
+            let imagesvideos = await primary.model(constants.MODELS.events, eventModel).find({ createdBy: { $ne: mongoose.Types.ObjectId(req.token.organizerid) }, is_live: true }).populate([
                 { path: "event_category", model: primary.model(constants.MODELS.categories, categorieModel), select: "category_name" },
                 { path: "createdBy", model: primary.model(constants.MODELS.organizers, organizerModel), select: "name profile_pic about" },
             ]).select("-__v -othercost -services -equipments -updatedBy -discounts -capcity -companydetail -tandc").lean();
