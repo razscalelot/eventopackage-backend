d50d694d029c4a4b7385fb203239e2c27a217fcd
razscalelot <you@example.com>
2023-09-01 18:55:19 +0530

    gallery add pagination at organizer

--- routes/organizers/gallery.js
+++ routes/organizers/gallery.js
@@ -77,7 +77,7 @@ router.post('/', helper.authenticateToken, async (req, res) => {
         let organizerData = await primary.model(constants.MODELS.organizers, organizerModel).findById(req.token.organizerid).select('-password').lean();
         if (organizerData && organizerData.status == true && organizerData.mobileverified == true && organizerData.is_approved == true) {
             let primary = mongoConnection.useDb(constants.DEFAULT_DB);
-            const { page, limit } = req.body;
+            const { page, limit, type } = req.body;
             if (page && page != '' && parseInt(page) != 0 && !isNaN(parseInt(page)) && limit && limit != '' && parseInt(limit) != 0 && !isNaN(parseInt(limit))) {
                 let imagesvideos = await primary.model(constants.MODELS.events, eventModel).find({ createdBy: { $ne: mongoose.Types.ObjectId(req.token.organizerid) }, is_live: true }).populate([
                     { path: "event_category", model: primary.model(constants.MODELS.categories, categorieModel), select: "category_name" },
@@ -86,40 +86,79 @@ router.post('/', helper.authenticateToken, async (req, res) => {
                 let allEventsImageVideo = [];
                 async.forEachSeries(imagesvideos, (imagevideo, next_imagevideo) => {
                     if (imagevideo.photos && imagevideo.photos != '' || imagevideo.videos && imagevideo.videos != '') {
-                        async.forEachSeries(imagevideo.photos, (photo, next_photo) => {
-                            photo._id = imagevideo._id;
-                            photo.event_category = imagevideo.event_category;
-                            photo.createdBy = imagevideo.createdBy;
-                            photo.display_name = imagevideo.display_name;
-                            photo.event_type = imagevideo.event_type;
-                            photo.timestamp = imagevideo.timestamp;
-                            photo.status = imagevideo.status;
-                            photo.createdAt = imagevideo.createdAt;
-                            photo.updatedAt = imagevideo.updatedAt;
-                            photo.aboutplace = imagevideo.aboutplace;
-                            photo.personaldetail = imagevideo.personaldetail;
-                            photo.capacity = imagevideo.capacity;
-                            photo.type = 'photo';
-                            allEventsImageVideo.push(photo);
-                            next_photo();
-                        });
-                        async.forEachSeries(imagevideo.videos, (video, next_video) => {
-                            video._id = imagevideo._id;
-                            video.event_category = imagevideo.event_category;
-                            video.createdBy = imagevideo.createdBy;
-                            video.display_name = imagevideo.display_name;
-                            video.event_type = imagevideo.event_type;
-                            video.timestamp = imagevideo.timestamp;
-                            video.status = imagevideo.status;
-                            video.createdAt = imagevideo.createdAt;
-                            video.updatedAt = imagevideo.updatedAt;
-                            video.aboutplace = imagevideo.aboutplace;
-                            video.personaldetail = imagevideo.personaldetail;
-                            video.capacity = imagevideo.capacity;
-                            video.type = 'video';
-                            allEventsImageVideo.push(video);
-                            next_video();
-                        });
+                        if (type == 'photo') {
+                            async.forEachSeries(imagevideo.photos, (photo, next_photo) => {
+                                photo._id = imagevideo._id;
+                                photo.event_category = imagevideo.event_category;
+                                photo.createdBy = imagevideo.createdBy;
+                                photo.display_name = imagevideo.display_name;
+                                photo.event_type = imagevideo.event_type;
+                                photo.timestamp = imagevideo.timestamp;
+                                photo.status = imagevideo.status;
+                                photo.createdAt = imagevideo.createdAt;
+                                photo.updatedAt = imagevideo.updatedAt;
+                                photo.aboutplace = imagevideo.aboutplace;
+                                photo.personaldetail = imagevideo.personaldetail;
+                                photo.capacity = imagevideo.capacity;
+                                photo.type = 'photo';
+                                allEventsImageVideo.push(photo);
+                                next_photo();
+                            });
+                        } else if (type == 'video') {
+                            async.forEachSeries(imagevideo.videos, (video, next_video) => {
+                                video._id = imagevideo._id;
+                                video.event_category = imagevideo.event_category;
+                                video.createdBy = imagevideo.createdBy;
+                                video.display_name = imagevideo.display_name;
+                                video.event_type = imagevideo.event_type;
+                                video.timestamp = imagevideo.timestamp;
+                                video.status = imagevideo.status;
+                                video.createdAt = imagevideo.createdAt;
+                                video.updatedAt = imagevideo.updatedAt;
+                                video.aboutplace = imagevideo.aboutplace;
+                                video.personaldetail = imagevideo.personaldetail;
+                                video.capacity = imagevideo.capacity;
+                                video.type = 'video';
+                                allEventsImageVideo.push(video);
+                                next_video();
+                            });
+                        } else {
+                            async.forEachSeries(imagevideo.photos, (photo, next_photo) => {
+                                photo._id = imagevideo._id;
+                                photo.event_category = imagevideo.event_category;
+                                photo.createdBy = imagevideo.createdBy;
+                                photo.display_name = imagevideo.display_name;
+                                photo.event_type = imagevideo.event_type;
+                                photo.timestamp = imagevideo.timestamp;
+                                photo.status = imagevideo.status;
+                                photo.createdAt = imagevideo.createdAt;
+                                photo.updatedAt = imagevideo.updatedAt;
+                                photo.aboutplace = imagevideo.aboutplace;
+                                photo.personaldetail = imagevideo.personaldetail;
+                                photo.capacity = imagevideo.capacity;
+                                photo.type = 'photo';
+                                allEventsImageVideo.push(photo);
+                                next_photo();
+                            });
+                            async.forEachSeries(imagevideo.videos, (video, next_video) => {
+                                video._id = imagevideo._id;
+                                video.event_category = imagevideo.event_category;
+                                video.createdBy = imagevideo.createdBy;
+                                video.display_name = imagevideo.display_name;
+                                video.event_type = imagevideo.event_type;
+                                video.timestamp = imagevideo.timestamp;
+                                video.status = imagevideo.status;
+                                video.createdAt = imagevideo.createdAt;
+                                video.updatedAt = imagevideo.updatedAt;
+                                video.aboutplace = imagevideo.aboutplace;
+                                video.personaldetail = imagevideo.personaldetail;
+                                video.capacity = imagevideo.capacity;
+                                video.type = 'video';
+                                allEventsImageVideo.push(video);
+                                next_video();
+                            });
+                        }
+
                     }
                     next_imagevideo();
                 }, () => {
@@ -265,7 +304,7 @@ router.get('/mypost', helper.authenticateToken, async (req, res) => {
 router.post('/mypost', helper.authenticateToken, async (req, res) => {
     if (req.token.organizerid && mongoose.Types.ObjectId.isValid(req.token.organizerid)) {
         let primary = mongoConnection.useDb(constants.DEFAULT_DB);
-        const { page, limit } = req.body;
+        const { page, limit, type } = req.body;
         if (page && page != '' && parseInt(page) != 0 && !isNaN(parseInt(page)) && limit && limit != '' && parseInt(limit) != 0 && !isNaN(parseInt(limit))) {
             let organizerData = await primary.model(constants.MODELS.organizers, organizerModel).findById(req.token.organizerid).select('-password').lean();
             if (organizerData && organizerData.status == true && organizerData.mobileverified == true && organizerData.is_approved == true) {
