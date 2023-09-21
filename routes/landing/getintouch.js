919f26edd83025935de766b62bbb05e089c30e2d
razscalelot <you@example.com>
2023-09-21 10:58:41 +0530

    new landing page get in touch api

--- routes/landing/getintouch.js
+++ routes/landing/getintouch.js
@@ -50,4 +50,49 @@ router.post('/', async (req, res) => {
         return responseManager.badrequest({ message: 'Invalid data to send query, please try again' }, res);
     }
 });
+router.post('/newlanding', async (req, res) => {
+    res.setHeader('Access-Control-Allow-Headers','Content-Type,Authorization');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    const { name, phone, email, description } = req.body;
+    if (name && name.trim() != '' && email && email.trim() != '' && (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) && description && description.trim() != '') {
+        let primary = mongoConnection.useDb(constants.DEFAULT_DB);
+        let checkExisting = await primary.model(constants.MODELS.getintouches, getintouchModel).findOne({email: email}).lean();
+        if (checkExisting == null) {
+            let obj = {
+                name: name,
+                email: email,
+                phone: phone,
+                description: description,
+            };
+            await primary.model(constants.MODELS.getintouches, getintouchModel).create(obj);
+            const tranEmailApi = new Sib.TransactionalEmailsApi()
+            const sender = {
+                email: email,
+                name: name
+            }
+            const receivers = [
+                {
+                    email: process.env.SIB_EMAIL_ID,
+                    name: 'Festum Evento'
+                },
+            ];
+            tranEmailApi.sendTransacEmail({
+                sender,
+                to: receivers,
+                subject: name + ' Query ',
+                htmlContent: `<h3>`+ name +`</h3><br/><h5>`+ description +`</h5>`,
+            }).then((response) => {
+                console.log('success', response);
+                return responseManager.onSuccess('Thank you for getting in touch. we will reply by email as soon as possible.', 1, res);
+            }).catch((error) => {
+                console.log('error', error);
+                return responseManager.onError(error, res);
+            });
+        } else {
+            return responseManager.badrequest({ message: 'User already send to query with same email, Please try again...' }, res);
+        }
+    } else {
+        return responseManager.badrequest({ message: 'Invalid data to send query, please try again' }, res);
+    }
+});
 module.exports = router;
