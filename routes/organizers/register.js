73fe4993bd56619000ac090859456ce5e0e67795
razscalelot <you@example.com>
2023-10-11 18:04:40 +0530

    organizer profile delete api

--- routes/organizers/register.js
+++ routes/organizers/register.js
@@ -183,4 +183,22 @@ router.post('/resendotp', async (req, res) => {
         return responseManager.badrequest({ message: 'Invalid mobile number, please try again' }, res);
     }
 });
+router.post('/deleteorganizer', helper.authenticateToken, async (req, res) => {
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    console.log(req.token.organizerid);
+    if (req.token.organizerid && mongoose.Types.ObjectId.isValid(req.token.organizerid)) {
+        let primary = mongoConnection.useDb(constants.DEFAULT_DB);
+        let organizerData = await primary.model(constants.MODELS.organizers, organizerModel).findById(req.token.organizerid).lean();
+
+        if (organizerData && organizerData.status == true && organizerData.mobileverified == true) {
+            await primary.model(constants.MODELS.organizers, organizerModel).findByIdAndUpdate(req.token.organizerid, { status: false });
+            return responseManager.onSuccess('Organizer deleted successfully!', 1, res);
+        } else {
+            return responseManager.onSuccess('Invalid Organizer id to delete organizer, Please try again!', 0, res);
+        }
+    } else {
+        return responseManager.onSuccess('Invalid Organizer id to delete organizer, Please try again!', 0, res);
+    }
+});
 module.exports = router;
